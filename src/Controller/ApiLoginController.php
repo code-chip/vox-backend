17e7ef96e9c99778ee0ff08a06b5461ec12896c9
Code Chip <willvix@outlook.com>
2025-06-01 14:09:42 -0300

    Authetication adjustment.

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -1,19 +1,37 @@
 <?php
 
+/**
+ * @copyright  Copyright (c) 2025 Code Chip (https://codechip.com.br)
+ * @author     Will <willvix@outlook.com>
+ * @Link       https://github.com/code-chip
+ */
+
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class ApiLoginController extends AbstractController
 {
-    #[Route('/api/login', name: 'api_login')]
-    public function index(): JsonResponse
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): Response
     {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = 'token';;
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiLoginController.php',
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token,
         ]);
     }
 }
